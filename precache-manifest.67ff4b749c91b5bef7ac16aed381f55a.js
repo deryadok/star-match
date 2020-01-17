3164e382942b589514a35c09f2da64ebdb4c6c9d
Derya Dok <derya.dok@ogr.deu.edu.tr>
2020-01-17 15:00:58 +0300

    Updates

---
+++ precache-manifest.67ff4b749c91b5bef7ac16aed381f55a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac690dcdd64b72a8af568bb0af00eb7e",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/index.html"
+  },
+  {
+    "revision": "e3a7bb34f44c624e2a30",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/static/css/main.1111d916.chunk.css"
+  },
+  {
+    "revision": "1c160827a59da4153664",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/static/js/2.839bab63.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/static/js/2.839bab63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3a7bb34f44c624e2a30",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/static/js/main.1003ef58.chunk.js"
+  },
+  {
+    "revision": "b152a9442f8b3a734db9",
+    "url": "%7Bderyadok%7D.github.io/%7Bstar-match%7D/static/js/runtime-main.ec1bdf58.js"
+  }
+]);
