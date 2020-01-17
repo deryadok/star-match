096d190056f72beb050cc20101d651ff86cf3be2
Derya Dok <derya.dok@ogr.deu.edu.tr>
2020-01-17 15:14:15 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%7Bderyadok%7D.github.io/%7Bstar-match%7D/precache-manifest.67ff4b749c91b5bef7ac16aed381f55a.js"
+  "/star-match/precache-manifest.36211c8545965444e51897f5c7a79efd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%7Bderyadok%7D.github.io/%7Bstar-match%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/star-match/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
